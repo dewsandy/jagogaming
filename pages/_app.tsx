ca665c70ee1d668635cd2ff35260d504b3dc84fe
Dewi Sandy <dewi.sandyislamiati@gmail.com>
2022-01-19 08:21:21 +0700

    Update Atomic Design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
 import '../styles/utilities.css';
 import '../styles/homepage.css';
+import '../styles/detail.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-success.css';
+import '../styles/404-not-found.css';
+import '../styles/sidebar.css';
+import '../styles/overview.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
+import '../styles/navbar-log-in.css';
+
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
@@ -18,6 +33,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/* Custom CSS */}
         <link rel="stylesheet" href="./css/homepage.css" />
         <link rel="stylesheet" href="./css/utilities.css" />
+        <link rel="stylesheet" href="./css/detail.css" />
+        <link rel="stylesheet" href="./css/checkout.css" />
+        <link rel="stylesheet" href="./css/complete-checkout.css" />
+        <link rel="stylesheet" href="./css/sign-in.css" />
+        <link rel="stylesheet" href="./css/sign-up.css" />
+        <link rel="stylesheet" href="./css/sign-up-photo.css" />
+        <link rel="stylesheet" href="./css/sign-up-success.css" />
+        <link rel="stylesheet" href="./css/404-not-found.css" />
 
         {/* {Google Fonts} */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -28,7 +51,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         {/* {AOS Animation} */}
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
 
         {/* Boostrap JS */}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous" />
